d9e08aef254d23004d2b03a292c4d1f15ed2fa94
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-07-16 21:17:34 +0300

    latest

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+let myName = "";
+newHeader.textContent = myName + "is the champion";
